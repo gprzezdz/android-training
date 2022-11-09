382debb38d075ada5d178f6f156891ea75cfdea5
Grzegorz Przezdziecki <grzegorz@przezdziecki.pl>
2022-11-09 14:58:51 +0100

    replace Clock.systemDefaultZone().millis() by  System.currentTimeMillis()

--- AlarmManager/app/src/main/java/pl/przezdziecki/training/android/alarmmanager/MainActivity.kt
+++ AlarmManager/app/src/main/java/pl/przezdziecki/training/android/alarmmanager/MainActivity.kt
@@ -3,13 +3,11 @@ package pl.przezdziecki.training.android.alarmmanager
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.SystemClock
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import pl.przezdziecki.training.android.alarmmanager.databinding.ActivityMainBinding
-import java.time.Clock
 
 private var TAG: String = "MainActivity"
 class MainActivity : AppCompatActivity() {
@@ -23,14 +21,14 @@ class MainActivity : AppCompatActivity() {
         binding.btnSetAlarm.setOnClickListener {
             Log.d(TAG, "set alarm")
             val time: Int=binding.edtTime.text.toString().toInt()
-            val triggerTime= Clock.systemDefaultZone().millis()+(time*1000)
+            val triggerTime= System.currentTimeMillis()+(time*1000)
             val i = Intent(application, AlarmReceiver::class.java)
-            var pi: PendingIntent = PendingIntent.getBroadcast(
+            val pi: PendingIntent = PendingIntent.getBroadcast(
              applicationContext,
                 100, i, PendingIntent.FLAG_IMMUTABLE
             )
             alarmManager.set(AlarmManager.RTC_WAKEUP,triggerTime,pi)
-            Toast.makeText(applicationContext, "Alarm set", Toast.LENGTH_LONG).show();
+            Toast.makeText(applicationContext, "Alarm set", Toast.LENGTH_LONG).show()
         }
         alarmManager= getSystemService(ALARM_SERVICE) as AlarmManager
     }
